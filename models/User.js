e4563f5d98ea4f4047f343590af68a002ad3c567
Dan Leatherman <dleatherman@gmail.com>
2015-04-24 23:59:40 -0400

    initial commit

---
+++ models/User.js
@@ -0,0 +1,45 @@
+var bcrypt = require('bcrypt-nodejs');
+var crypto = require('crypto');
+var mongoose = require('mongoose');
+
+var userSchema = new mongoose.Schema({
+  email: { type: String, unique: true, lowercase: true },
+  password: String,
+
+  tokens: Array,
+
+  profile: {
+    name: { type: String, default: '' },
+  },
+
+  resetPasswordToken: String,
+  resetPasswordExpires: Date
+});
+
+/**
+ * Password hash middleware.
+ */
+userSchema.pre('save', function(next) {
+  var user = this;
+  if (!user.isModified('password')) return next();
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) return next(err);
+    bcrypt.hash(user.password, salt, null, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+/**
+ * Helper method for validating user's password.
+ */
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
+
+module.exports = mongoose.model('User', userSchema);
